910ea5e44279778181af06ec549a7bbb2a10338c
Andrew Pellum <apellum19@gmail.com>
2021-11-04 11:42:36 -0600

    Added Home Component with Route

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import NavBar from './NavBar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <Router>
+        <NavBar/>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>To Do List</h1>
+            <ol>
+                <li>Clean the House</li>
+            </ol>
+        </div>
+    )
+}
+
+export default Home
