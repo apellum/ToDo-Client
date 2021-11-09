7c2d366cb4c8cf886daf4eda31367cc0327dc4ab
Andrew Pellum <apellum19@gmail.com>
2021-11-08 20:16:26 -0700

    Added Create Todo Form

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
 import { Box, AppBar, Toolbar, Typography } from '@mui/material'
 
 
 const NavBar = () => {
     const loggedIn = useSelector(state => state.sessions.loggedIn)
-    console.log(loggedIn)
+    const currentUser = useSelector(state => state.sessions.currentUser)
     return (
         <div>
             <Box>
                 <AppBar>
                     <Toolbar>
-                        <Typography>Family ToDo App</Typography>
+                        {loggedIn ? <Typography>{currentUser.first_name}</Typography> : <Typography>Family ToDo App</Typography>}
+                        <ul>
+                            <li><NavLink to="/">Home</NavLink></li>
+                            <li><NavLink to="/new-todo">New Todo</NavLink></li>
+                            <li><NavLink to="/signup">Signup</NavLink></li>
+                            <li><NavLink to="/login">Login</NavLink></li>
+                        </ul>
                     </Toolbar>
                 </AppBar>
             </Box>
